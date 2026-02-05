6e1e0a8274f2c1f6a0b5994dc72b48149f25e470
bojidar <bojidar@mail.com>
2026-02-05 18:31:02 +0200

    Initial Release.

--- obj/Debug/net10.0/osx-x64/DiplomnaRabotaConsole.AssemblyInfo.cs
+++ obj/Debug/net10.0/osx-x64/DiplomnaRabotaConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd899de310475112d0dee4887a25bf78a02a03c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
