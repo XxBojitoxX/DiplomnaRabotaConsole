a004f1829750da2af4fddd12c1214c539da7c61c
XxBojitoxX <bojit11032007@gmail.com>
2026-03-18 21:03:53 +0200

    Updated code to remove incompatible emojis from source code

--- obj/Debug/net10.0-windows/DiplomnaRabotaConsole.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/DiplomnaRabotaConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d99be34826940641f9c00768f56fbf44339a357")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a30e325fb6bfec7fc8d349001908c0d18983177")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
