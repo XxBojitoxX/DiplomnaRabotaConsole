bcbc37459116f683be90d9b172a800e61b0ed401
XxBojitoxX <bojit11032007@gmail.com>
2026-03-19 16:49:26 +0200

    sourcelink 5 pm + pending release push

--- obj/Debug/net10.0/win-x64/DiplomnaRabotaConsole.AssemblyInfo.cs
+++ obj/Debug/net10.0/win-x64/DiplomnaRabotaConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9716267f89fe135eb67a721edbe7f328995e7ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f2b12cd6389783aab30fdf6fb988c984a69f5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net10.0/win-x64/DiplomnaRabotaConsole.AssemblyInfo.cs
+++ obj/Release/net10.0/win-x64/DiplomnaRabotaConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f2b12cd6389783aab30fdf6fb988c984a69f5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiplomnaRabotaConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
